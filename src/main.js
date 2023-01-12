127b42554267f095e42af67b81bdbd54890b18ce
tuva9 <tuva9@fpt.edu.vn>
2023-01-12 09:06:09 +0700

    Initialize project

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import HomePage from "./pages/home"
+
+// DOM declaration
+var app = document.querySelector('#app')
+
+app.innerHTML = HomePage()
+
---
+++ src/pages/home.js
@@ -0,0 +1,5 @@
+var HomePage = function() {
+    return "<div><h1>Home page</h1><p>Website ban sach truc tuyen</p></div>"
+}
+
+export default HomePage
