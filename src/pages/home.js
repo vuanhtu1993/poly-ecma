54f602662095b73d36949e133e305cfc73007a95
tuva9 <tuva9@fpt.edu.vn>
2023-01-12 09:07:29 +0700

    Create pages

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,9 @@
 var HomePage = function() {
-    return "<div><h1>Home page</h1><p>Website ban sach truc tuyen</p></div>"
+    return /*html*/`
+    <div>
+        <h1>Home page</h1>
+        <p>Website ban sach truc tuyen</p>
+    </div>`
 }
 
 export default HomePage
