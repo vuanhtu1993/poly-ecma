b34a8b173e3c750d77440e6e907a09cecc4656d0
tuva9 <tuva9@fpt.edu.vn>
2023-01-29 22:12:57 +0700

    1. Not found page 2. Navigo

--- src/main.js
+++ src/main.js
@@ -2,9 +2,24 @@ import { render } from "../lib"
 import HomePage from "./pages/home"
 import ProductPage from "./pages/products"
 import './styles/main.css'
+import Navigo from "navigo"
+import NotFoundPage from "./pages/notFound"
+
+const router = new Navigo("/", {linksSelector: "a"})
 
 // DOM declaration
 var app = document.querySelector('#app')
 
-render(ProductPage(), app)
+router.on("/", function() {
+    render(HomePage(), app)
+})
+router.on("/products", function() {
+    render(ProductPage(), app)
+})
+
+router.notFound(function() {
+    render(NotFoundPage(), app)
+})
+router.resolve()
+
 
---
+++ src/pages/notFound.js
@@ -0,0 +1,5 @@
+const NotFoundPage = () => {
+    return `<h1>404 Not Found 😓</h1>`
+}
+
+export default NotFoundPage
