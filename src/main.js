e6861c7c230356e4b404cf89efc1c591795cc5eb
tuva9 <tuva9@fpt.edu.vn>
2023-01-31 17:23:32 +0700

    1. Render shared function 2. Products page

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import { render } from "../lib"
 import HomePage from "./pages/home"
+import ProductPage from "./pages/products"
 
 // Khai bao DOM
 var app = document.querySelector("#app")
 
-app.innerHTML = HomePage()
+// app.innerHTML = ProductPage()
+render(ProductPage(), app)
 
