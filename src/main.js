4a23c07a93e65d13206ece48cc8255d46ce61eea
tuva9 <tuva9@fpt.edu.vn>
2023-02-01 15:21:29 +0700

    1. Install navigo 2. Navigation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
+import { render } from "../lib"
 import HomePage from "./pages/home"
 import ProductPage from "./pages/products"
+import Navigo from "navigo"
 
 // DOM declaration
 var app = document.querySelector('#app')
+
+const router = new Navigo()
+
+router.on('/', function() {
+    render(HomePage(), app)
+})
+
+router.on('/products', function() {
+    render(ProductPage(), app)
+})
+
+router.resolve()
+
+
 
