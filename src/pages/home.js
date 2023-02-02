4f9eabdf77490b1b0ea8952ea2b9ac248b93b7cf
tuva9 <tuva9@fpt.edu.vn>
2023-02-02 18:07:49 +0700

    Buổi 8: Render product data

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,23 @@
 import Navigation from "../components/navigation"
+import data from '../../db.json' assert {type: 'json'}
 
 var HomePage = function() {
-    return `
+    console.log(data);
+    return /*html*/`
     ${Navigation()}
     <h1>Trang chủ</h1>
-    <p>Website bán sách trực tuyến</p>`
+    <p>Website bán sách trực tuyến</p>
+    <div class="grid grid-cols-4 gap-2">
+        ${data.map(function(book, index) {
+            return /*html*/`
+                <div>
+                <img src="${book.images[0].base_url}"/>
+                <h3>${book.name}</h3>
+                </div>
+            `
+        }).join('')}
+    </div>
+    `
 }
 
 export default HomePage
