6f5d8835e32c12a3a41e972f616ad833401670a1
tuva9 <tuva9@fpt.edu.vn>
2023-02-02 16:49:54 +0700

    Buổi 8

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,7 +1,7 @@
 const Navigation = function() {
     return /*html*/`
         <ul class="flex gap-2">
-            <li><a class="hover:text-red-500" href="/">Trang chủ</a></li>
+            <li><a class="hover:text-red-500 bg-blue-500" href="/">Trang chủ</a></li>
             <li><a class="hover:text-red-500" href="/products">Sản phẩm</a></li>
             <li><a class="hover:text-red-500" href="#">Liên hệ</a></li>
             <li><a class="hover:text-red-500" href="#">Giỏ hàng</a></li>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,23 @@
 import Navigation from "../components/navigation"
+import data from '../../db.json' assert {type: "json"}
 
 var HomePage = function() {
+    console.log(data);
     return /*html*/`
     ${Navigation()}
     <div>
         <h1>Home page</h1>
         <p>Website ban sach truc tuyen</p>
+        <div class="grid grid-cols-4 gap-2">
+            ${data.map(function(book, index) {
+                return /*html*/`
+                <div>
+                    <img src="${book.images[0].base_url}"/>
+                    <h3>${book.name}</h3>
+                </div>
+                `
+            }).join('')}
+        <div>
     </div>`
 }
 
