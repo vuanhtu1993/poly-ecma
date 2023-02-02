9ffa3e96e9a3bc1e974bae90b37ad9569438d59d
tuva9 <tuva9@fpt.edu.vn>
2023-02-02 07:51:09 +0700

    Integrated tailwind

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,10 +1,10 @@
 const Navigation = function() {
     return /*html*/`
-        <ul class="flex unStyleList gap-2">
-            <li><a href="/">Trang chủ</a></li>
-            <li><a href="/products">Sản phẩm</a></li>
-            <li><a href="#">Liên hệ</a></li>
-            <li><a href="#">Giỏ hàng</a></li>
+        <ul class="flex gap-2">
+            <li><a class="hover:text-red-500" href="/">Trang chủ</a></li>
+            <li><a class="hover:text-red-500" href="/products">Sản phẩm</a></li>
+            <li><a class="hover:text-red-500" href="#">Liên hệ</a></li>
+            <li><a class="hover:text-red-500" href="#">Giỏ hàng</a></li>
         </ul>
     `
 }
