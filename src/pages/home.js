55ac9ec939732aeb42a780df7cb5e6d7eeb28e35
tuva9 <tuva9@fpt.edu.vn>
2023-02-03 08:25:42 +0700

    Buổi 8: render data

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,23 @@
 import Navagation from "../components/navigation"
+import data from '../../db.json' assert {type: 'json'}
 
 var HomePage = function() {
+    console.log(data);
     return /*html*/`
     ${Navagation()}
     <div>
         <h1>Home page</h1>
         <p>Website ban sach truc tuyen</p>
+        <div>
+        ${data.map(function(book, index) {
+            return /*html*/`
+            <div>
+                <img src="${book.images[0].base_url}"/>
+                <h3>${book.name}</h3>
+            </div>
+            `
+        }).join('')}
+        </div>
     </div>`
 }
 
