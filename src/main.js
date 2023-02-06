a3e602694c38b3b390196d053b2eaf011e45be2a
tuva9 <tuva9@fpt.edu.vn>
2023-02-06 14:55:51 +0700

    Buổi 9: Product detail page

--- src/main.js
+++ src/main.js
@@ -13,8 +13,8 @@ router.on('/', function() {
     render(HomePage(), app)
 })
 
-router.on('/products', function() {
-    render(ProductPage(), app)
+router.on('/products/:id', function({data}) {
+    render(ProductPage(data.id), app)
 })
 
 router.resolve()
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,9 +1,21 @@
 import Navigation from "../components/navigation";
+import data from '../../db.json' assert {type: "json"}
 
-var ProductPage = function() {
-    return `
+const urlParam = new URLSearchParams()
+console.log(urlParam.get('id'));
+
+var ProductPage = function(id) {
+    console.log(id);
+    const book = data.find(function(item) {
+        return item.id == id
+    })
+    return /*html*/`
     ${Navigation()}
-    <h1>Product page</h1>
+    <h1>Product page ${id}</h1>
+    <div>
+        <img class="w-1/2" src="${book.images[0].base_url}"/>
+        <h3>${book.name}</h3>
+    </div>
     `
 }
 
