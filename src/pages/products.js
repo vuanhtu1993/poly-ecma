a59efaba79c66c65ee319a55d978ce7fa18c1032
tuva9 <tuva9@fpt.edu.vn>
2023-02-16 18:09:57 +0700

    Buổi 11: Related book

--- src/pages/products.js
+++ src/pages/products.js
@@ -3,7 +3,10 @@ import { useEffect, useState } from "../../lib";
 import Navigation from "../components/navigation";
 
 const ProductPage = function(id) {
+    const param = new URLSearchParams(location.search)
+    const id_cate = param.get('id_categories')
     const [book, setBook] = useState({})
+    const [relatedBook, setRelatedBook] = useState([])
 
     useEffect(function() {
         axios.get(`http://localhost:3000/books/${id}`)
@@ -11,6 +14,16 @@ const ProductPage = function(id) {
             setBook(dataAxios.data)
         })
     }, [])
+
+    useEffect(function() {
+        axios.get(`http://localhost:3000/books?categories.id=${id_cate}`)
+        .then(function(data) {
+            setRelatedBook(data.data)
+        })
+    }, [])
+
+    console.log(relatedBook);
+
     // Optional chain
     return /*html*/`
     ${Navigation()}
