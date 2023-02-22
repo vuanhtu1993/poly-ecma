9d91311520831236f83447b42d1b697e063f1303
tuva9 <tuva9@fpt.edu.vn>
2023-02-22 15:45:17 +0700

    Apply bootstrap 5

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
 import { render, router } from "../lib"
 import './style/main.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 // DOM declaration
 var app = document.querySelector('#app')
 
 router.on('/', function() {
-    render(() => `<h1 class="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">Base EMCA project</h1>`, app)
+    render(() => /*html*/`
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    `, app)
 })
 
 router.resolve()
